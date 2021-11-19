6d4df6bc721c59dbe726e3ad97f5727cd266f17a
adammorphy <89942442+adammorphy@users.noreply.github.com>
2021-11-18 16:54:09 -0800

    Update demo.R

--- src/demo.R
+++ src/demo.R
@@ -2,7 +2,7 @@
 # date: 2020-01-15
 
 "This script prints out docopt args.
-Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>]
+Usage: demo.R <arg1> --arg2=<arg2> [--arg3=<arg3>] [--arg4=<arg4>]
 Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
@@ -13,12 +13,8 @@ Options:
 library(docopt)
 opt <- docopt(doc)
 
-main <- function(opt) {
-	
-  print(opt)
-  print(opt$arg4)
-  print(typeof(opt))
-	
-}
 
-main(opt)
+	
+print(opt)
+print(opt$arg4)
+print(typeof(opt))
