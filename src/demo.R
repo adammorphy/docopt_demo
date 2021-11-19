4bd5db848111b41b4641e3581866ece59509aa30
adammorphy <89942442+adammorphy@users.noreply.github.com>
2021-11-18 16:44:12 -0800

    Update demo.R

--- src/demo.R
+++ src/demo.R
@@ -7,9 +7,11 @@ Options:
 <arg>             Takes any value (this is a required positional argument)
 --arg2=<arg2>     Takes any value (this is a required option)
 [--arg3=<arg3>]   Takes any value (this is an optional option)
+[--arg4=<arg4>]   Takes any value (this is an optional option)
 " -> doc
 
 library(docopt)
 opt <- docopt(doc)
 print(opt)
+print(opt$arg4)
 print(typeof(opt))
